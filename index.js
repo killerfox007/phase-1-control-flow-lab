ff55074317f0ac4e66cf66acddefbb11526ca3dd
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-13 16:21:22 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feets){
+  if (feets <= 400){
+  return "This one is on me!"
+}
+else if (feets > 400 && feets <= 2000){
+    return "That will be twenty bucks." 
+} else if (feets > 2000 && feets < 2500){
+  return "I will gladly take your thirty bucks."
+} else {
+  return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+} else {
+  return "No go."
+}
+}
+function switchOnCharmFromTip(tipping){
+  if (tipping ==="generous"){
+    return "Thank you so much."
+} else if (tipping === 'not as generous'){
+  return "Thank you."
+} else {
+  return "Bye."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
